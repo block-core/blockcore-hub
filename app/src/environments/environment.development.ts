88dba10dbb14fc169dc33e1b3489880e6fd7940c
SondreB <sondre@outlook.com>
2023-10-11 22:37:30 +0200

    Setup API proxy
    - This fixes issue with cookie auth

--- app/src/environments/environment.development.ts
+++ app/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5050/api',
+  production: false,
+  apiUrl: 'http://localhost:4200/api',
 };
--- srv/services/verifyUser.mjs
+++ srv/services/verifyUser.mjs
@@ -1,27 +1,10 @@
-import express from "express";
-import db from "../db/conn.mjs";
-import MUUID from "uuid-mongodb";
-import cache from "memory-cache";
-import { serialize } from "cookie";
 import jwt from "jsonwebtoken";
-import { Resolver } from "did-resolver";
-import is from "@blockcore/did-resolver";
-import { verifyJWS } from "did-jwt";
-
-const mUUID = MUUID.mode("relaxed"); // use relaxed mode
-const ADMINS = process.env["ADMIN"]?.split(",").filter((i) => i.trim());
-const PRODUCTION = process.env["NODE_ENV"] === "production";
 const KEY = process.env["JWT_KEY"];
-const APIKEY = process.env["API_KEY"];
-const router = express.Router();
 
 export function verifyUser(req, res) {
   try {
     const { cookies } = req;
     const token = cookies.token;
-
-    console.log("Verifying user... token:", token);
-    console.log("Verifying user... cookies:", cookies);
 
     if (!token) {
       res
